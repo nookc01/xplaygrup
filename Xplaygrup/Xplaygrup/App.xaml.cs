42268276abd074140b97e8bd47069f0f305f7c40
nookc01 <chivisailam01@gmail.com>
2023-11-07 16:35:47 +0100

    xplaygrup

---
+++ Xplaygrup/Xplaygrup/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace xplaygrup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
